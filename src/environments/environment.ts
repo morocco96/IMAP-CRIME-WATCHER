185f5649438cd900683b925a1caf72b578dc607f
morocco96 <moroccotheboy@gmail.com>
2022-11-29 16:54:02 +0100

    Fix update logo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080'
+  baseUrl: '192.168.43.228'
 };
 
 /*
